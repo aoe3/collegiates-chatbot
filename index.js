f8b814ebe683ad727a6b4ab4898bae1c02b6e24c
Andrew Eccles <aoe3@cornell.edu>
2018-03-19 15:08:31 -0400

    change index.js for hub_challenge

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send($_GET(["hub_challenge"]));
+	res.send(req.query['hub.challenge'])
 })
 
 // for Facebook verification
