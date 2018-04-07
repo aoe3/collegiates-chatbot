a28804a7a05bfc36632ea937dd0329dd3b692f4a
Andrew Eccles <aoe3@cornell.edu>
2018-04-07 18:39:59 -0400

    final pre-tournament commit

--- index.js
+++ index.js
@@ -1587,7 +1587,7 @@ function decideMessage(sender, textInput){
 
 
 	} else if (text == "schedule"){
-		sendText(sender, "Ring 1: ADV Female Spear. \nRing 2: ADV Female Soft Weapon. \nRing 3: Male Open Chen Taijiquan.")
+		sendText(sender, "Ring 1: ADV Male Spear. \nRing 2: Finished. \nRing 3: Female Open Chen Taijiquan.")
 
 
 
