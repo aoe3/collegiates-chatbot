a2b135d918fefbf605a5b7dc6d95bf9a32d09c1b
Andrew Eccles <aoe3@cornell.edu>
2018-04-07 13:05:56 -0400

    final pre-tournament commit

--- index.js
+++ index.js
@@ -1585,7 +1585,7 @@ function decideMessage(sender, textInput){
 
 
 	} else if (text == "schedule"){
-		sendText(sender, "Ring 1: ADV Male NQ. \nRing 2: Warmups. \nRing 3: BEG Female CQ.")
+		sendText(sender, "Ring 1: ADV Female Traditional Open Barehand. \nRing 2: Warmups. \nRing 3: Warmup.")
 
 
 
