71cfb80a4a2f0730767c5f78087347e6c084161c
Andrew Eccles <aoe3@cornell.edu>
2018-04-07 13:53:26 -0400

    final pre-tournament commit

--- index.js
+++ index.js
@@ -1585,7 +1585,7 @@ function decideMessage(sender, textInput){
 
 
 	} else if (text == "schedule"){
-		sendText(sender, "Ring 1: Lunch/Open Mat. \nRing 2: Lunch/Open Mat. \nRing 3: Lunch/Open Mat.")
+		sendText(sender, "Ring 1: Lunch/Open Mat. \nRing 2: INT Female NQ. \nRing 3: BEG Male NQ.")
 
 
 
